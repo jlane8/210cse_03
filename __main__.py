1da89c1720b12eeba11f2fc76d40587fe33806da
jlane8 <113236803+jlane8@users.noreply.github.com>
2022-09-28 15:30:11 -0500

    added comments

--- __main__.py
+++ __main__.py
@@ -5,7 +5,9 @@ Purpose:
     The __main__.py file will launch the Jumper game by creating a Jumper
     instantiation and calling the start_game method on it.
 """
+# import the Jumper class to instantiate the Jumper
 from game.jumper import Jumper
 
+# create Jumper instantiation, call the Jumper start_game method
 jumper = Jumper()
 jumper.start_game()
